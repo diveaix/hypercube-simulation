c58dcaad33420ea70df11d87f1ac4e97e00106f8
diveaix <dive.aix@gmail.com>
2025-06-11 20:09:09 +0530

    script.js

--- script.js
+++ script.js
@@ -193,7 +193,7 @@ function goToNextNode() {
 
 function showCompletionMessage() {
   const tweetText = encodeURIComponent(
-    "🧠 Just completed the Hypercube Simulator on-chain!\n\n4 nodes. 4 puzzles. 1 ZK Proof.\n\nI proved I'm not a Sybil 🌀 \nRun your hypercube simulator\n\nhypercube-simulation.vercel.app"
+    "🧠 Just completed the Hypercube Simulator on-chain!\n\n4 nodes. 4 puzzles. 1 ZK Proof.\n\nI proved I'm not a Sybil 🌀\n \nRun your hypercube simulator\nLink: hypercube-simulation.vercel.app"
   );
   const tweetUrl = `https://twitter.com/intent/tweet?text=${tweetText}`;
 
